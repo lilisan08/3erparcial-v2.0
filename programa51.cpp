50667d0db469e6da3a2bad35db238f9eed3b63c4
Lilian Yared Sanchez Hernandez <168390254+lilisan08@users.noreply.github.com>
2024-06-05 12:43:57 -0600

    Add files via upload

---
+++ programa51.cpp
@@ -0,0 +1,52 @@
+   #include <iostream>
+#include <cstring>
+
+int main() {
+    char n[20];
+    char contra[20];
+    int precioUnitario, cantidad, ventas;
+    int intentos = 3;
+    int total = 0;
+
+    do {
+        std::cout << "Introduce tu nombre: ";
+        std::cin >> n;
+
+        std::cout << "Introduce tu contraseña: ";
+        std::cin >> contra;
+
+        if (strcmp(n, "lilian") == 0 && strcmp(contra, "2b38") == 0) {
+            std::cout << "Bienvenido al sistema.\n"; 
+            break;
+        } else {
+            intentos--;
+            if (intentos > 0) {
+                std::cout << "Nombre de usuario o contraseña incorrectos. Te quedan " << intentos << " intentos.\n";
+            } else {
+                std::cout << "Se ha superado el número de intentos permitido.\n";
+                break;
+            }
+        }
+    } while (intentos > 0);
+    std::cout << "Digite el número de ventas del dia:";
+    std::cin >> ventas;
+    for (int i = 0; i < ventas; ++i)
+    
+        std::cout << "Ingresa la cantidad de los artículos: "; 
+        std::cout << "Para terminar, introduce la cantidad de 0 \n\n";
+        std::cin >> cantidad;
+        if (cantidad == 0) {
+            
+        }
+        std::cout << "Ingresa el precio unitario del artículo:\n ";
+        std::cin >> precioUnitario;
+        if (cantidad < 0 || precioUnitario < 0) {
+            std::cout << "Lo siento, las cantidades no pueden ser negativas. Ingresa otra cantidad.\n\n";
+        } else {
+            total += cantidad * precioUnitario;
+        }
+    
+    std::cout << "Tu total de los productos es de " << total << " pesos\n";
+
+    return 0;
+}
